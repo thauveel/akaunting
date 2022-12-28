9a022e9459c85b1c39a9f0e9db096198c3355423
thauveel <thauveel@gmail.com>
2022-12-28 13:52:28 +0500

    check php v

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,20 +1,20 @@
 <?php
 
 // Define minimum supported PHP version
-define('AKAUNTING_PHP', '8.0.2');
+define('AKAUNTING_PHP', '8.1.13');
 
 // Check PHP version
-if (version_compare(PHP_VERSION, AKAUNTING_PHP, '<')) {
-    $message = 'Error: Ask your hosting provider to use PHP ' . AKAUNTING_PHP . ' or higher for HTTP, CLI, and php command.' . PHP_EOL . PHP_EOL . 'Current PHP version: ' . PHP_VERSION . PHP_EOL;
+// if (version_compare(PHP_VERSION, AKAUNTING_PHP, '<')) {
+//     $message = 'Error: Ask your hosting provider to use PHP ' . AKAUNTING_PHP . ' or higher for HTTP, CLI, and php command.' . PHP_EOL . PHP_EOL . 'Current PHP version: ' . PHP_VERSION . PHP_EOL;
 
-    if (defined('STDOUT')) {
-        fwrite(STDOUT, $message);
-    } else {
-        echo($message);
-    }
+//     if (defined('STDOUT')) {
+//         fwrite(STDOUT, $message);
+//     } else {
+//         echo($message);
+//     }
 
-    die(1);
-}
+//     die(1);
+// }
 
 // Load composer for core
 require __DIR__ . '/../vendor/autoload.php';
